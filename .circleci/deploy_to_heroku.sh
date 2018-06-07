66784de7cf738c1172b0fdcc6c82bbf1de7203bf
Andrew Wei <andrewscwei@gmail.com>
2018-06-06 19:40:26 -0700

    Test build

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -4,25 +4,28 @@ set -e
 
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
-# cat > ~/.netrc << EOF
-# machine api.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
-# machine git.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
-# EOF
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine registry.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
 
-# # Add heroku.com to the list of known hosts
-# ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
 
 # # Deploy to Heroku container registry
 # # heroku plugins:install @heroku-cli/plugin-container-registry
-# heroku container:login
-# heroku container:push release --app=$CIRCLE_PROJECT_REPONAME
-docker login -u "$HEROKU_LOGIN" -p "$HEROKU_API_KEY" registry.heroku.com
-docker build --rm=false --build-arg NODE_ENV=production --build-arg PUBLIC_PATH=$PUBLIC_PATH -t registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:$CIRCLE_SHA1 .
-docker push registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:$CIRCLE_SHA1
+heroku container:login
+heroku container:push release --app=$CIRCLE_PROJECT_REPONAME
+# docker login -u "$HEROKU_LOGIN" -p "$HEROKU_API_KEY" registry.heroku.com
+# docker build --rm=false --build-arg NODE_ENV=production --build-arg PUBLIC_PATH=$PUBLIC_PATH -t registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:$CIRCLE_SHA1 .
+# docker push registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:$CIRCLE_SHA1
 
 echo
 echo "Successfuly deployed to Heroku"
