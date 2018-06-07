f2acd55911f7bbd41c71a100e9fe7179ea8b54cb
Andrew Wei <andrewscwei@gmail.com>
2018-06-06 20:15:59 -0700

    Test build

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -4,17 +4,14 @@ set -e
 
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
-# cat > ~/.netrc << EOF
-# machine api.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
-# machine git.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
-# machine registry.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
-# EOF
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_KEY
+EOF
 
 # Add heroku.com to the list of known hosts
 # ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
