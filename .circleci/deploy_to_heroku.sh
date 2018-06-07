dbcca1d9c359d177b79ce412bd06765ec3892c52
Andrew Wei <andrewscwei@gmail.com>
2018-06-06 19:27:03 -0700

    Test build

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-source .circleci/get_opts.sh
-
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
@@ -21,7 +19,7 @@ EOF
 # Add heroku.com to the list of known hosts
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
 
-heroku container:push release
+heroku container:push release --app=$CIRCLE_PROJECT_REPONAME
 
 echo
 echo "Successfuly deployed to Heroku"
