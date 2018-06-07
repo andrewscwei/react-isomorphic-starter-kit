89bbbc14f93101a4bdae775341d465d00036f264
Andrew Wei <andrewscwei@gmail.com>
2018-06-06 20:24:22 -0700

    Test build

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -13,17 +13,13 @@ machine git.heroku.com
   password $HEROKU_KEY
 EOF
 
-# Add heroku.com to the list of known hosts
-# ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
 
-# # Deploy to Heroku container registry
-# # heroku plugins:install @heroku-cli/plugin-container-registry
-# heroku container:login
-# heroku container:push web --app=$CIRCLE_PROJECT_REPONAME
-# heroku container:release web --app=$CIRCLE_PROJECT_REPONAME
 docker login -u "$HEROKU_LOGIN" -p "$HEROKU_KEY" registry.heroku.com
-docker build --rm=false --build-arg NODE_ENV=production --build-arg PUBLIC_PATH=$PUBLIC_PATH -t registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:$CIRCLE_SHA1 .
-docker push registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:$CIRCLE_SHA1
+docker build --rm=false --build-arg NODE_ENV=production --build-arg PUBLIC_PATH=$PUBLIC_PATH -t registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:latest .
+docker push registry.heroku.com/$CIRCLE_PROJECT_REPONAME/web:latest
+
+heroku container:login
+heroku container:release web --app=react-isomorphic-starter-kit
 
 echo
 echo "Successfuly deployed to Heroku"
