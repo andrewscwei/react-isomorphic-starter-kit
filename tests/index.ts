4d126f44fcabb0f4bc369d5dd933ca44ca0694a8
Andrew Wei <andrewscwei@gmail.com>
2020-12-07 16:09:04 -0800

    Fix tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,21 +1,23 @@
-import assert from 'assert';
-import fs from 'fs';
-import { describe, it } from 'mocha';
-import path from 'path';
-import request from 'supertest';
+/* eslint-disable no-console */
 
-const baseDir = path.join(__dirname, '../build');
-const app = require(path.join(baseDir, 'server')).default;
+import assert from 'assert'
+import fs from 'fs'
+import { describe, it } from 'mocha'
+import path from 'path'
+import request from 'supertest'
+
+const baseDir = path.join(__dirname, '../build')
+const app = require(path.join(baseDir, 'index')).default
 
 describe('app', () => {
   before(() => {
-    console.log(`Verifying built files at ${baseDir}...`);
-    assert(fs.existsSync(baseDir));
-    console.log(`Verifying built files at ${baseDir}... OK`);
-  });
+    console.log(`Verifying built files at ${baseDir}...`)
+    assert(fs.existsSync(baseDir))
+    console.log(`Verifying built files at ${baseDir}... OK`)
+  })
 
   it('can ping /', async () => {
-    const res = await request(app).get('/');
-    assert(res.status === 200);
-  });
-});
+    const res = await request(app).get('/')
+    assert(res.status === 200)
+  })
+})
