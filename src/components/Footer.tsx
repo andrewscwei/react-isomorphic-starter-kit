667401638d65974a8cb95dabd39bda9ae6f307c4
Andrew Wei <andrewscwei@gmail.com>
2021-10-31 19:22:38 -0700

    Improve scripts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, PropsWithChildren } from 'react'
+import React, { FunctionComponent } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import $$GitHubIcon from '../assets/images/github-icon.svg'
