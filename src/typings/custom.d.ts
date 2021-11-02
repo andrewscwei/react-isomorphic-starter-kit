e447834c50db421714d80811443ee364a7321128
Andrew Wei <andrewscwei@gmail.com>
2021-11-01 17:34:07 -0700

    Improve typings

--- src/typings/custom.d.ts
+++ src/typings/custom.d.ts
@@ -1,17 +1,12 @@
-declare module 'webpack-manifest-plugin'
-declare module 'sitemap'
+// Suports importing assets.
 declare module '*.svg'
 declare module '*.jpg'
 declare module '*.png'
 
-declare const __BUILD_CONFIG__: Record<string, any>
-declare const __ASSET_MANIFEST__: AssetManifest
-declare const __I18N_CONFIG__: Readonly<{
-  defaultLocale: string
-  locales: string
-  dict: TranslationDataDict
-}>
+// Supports typing for build config.
+declare const __BUILD_CONFIG__: typeof import('../../config/build.conf').default
 
+// Supports web workers.
 declare module 'worker-loader!*' {
   class WebpackWorker extends Worker {
     constructor()
@@ -20,18 +15,36 @@ declare module 'worker-loader!*' {
   export default WebpackWorker
 }
 
-type TranslationData = { [key: string]: string | TranslationData }
+// Supports I18n.
+interface TranslationData { [key: string]: TranslationData | string }
 type TranslationDataDict = Record<string, TranslationData>
+
+declare const __I18N_CONFIG__: Readonly<{
+  defaultLocale: string
+  locales: string
+  dict: TranslationDataDict
+}>
+
+// Supports asset manifest file.
+declare module 'webpack-manifest-plugin'
+
 type AssetManifest = Record<string, any>
 
+declare const __ASSET_MANIFEST__: AssetManifest
+
+// Supports sitemap generation.
+declare module 'sitemap'
+
+// Supports custom error with status code.
+interface Error {
+  status?: number
+}
+
+// Custom window properties.
 interface Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: () => void
   __BUILD_NUMBER__: string
   __INITIAL_STATE__?: Record<string, any>
   __LOCALS__: Record<string, any>
   __VERSION__: string
-}
-
-interface Error {
-  status?: number
 }
--- src/typings/styled.d.ts
+++ src/typings/styled.d.ts
@@ -1,5 +1,6 @@
 import * as theme from '../styles/theme'
 
+// Supports typing for Styled Components theme.
 type Theme = typeof theme
 
 declare module 'styled-components' {
