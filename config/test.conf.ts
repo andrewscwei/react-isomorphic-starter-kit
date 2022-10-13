cab1988412cf8058f36dc87ebc7bb2b492f01161
Andrew Wei <andrewscwei@gmail.com>
2022-10-13 02:16:01 -0700

    Update tests

--- config/test.conf.ts
+++ config/test.conf.ts
@@ -11,7 +11,7 @@ const config: Config = {
     __BUILD_ARGS__: buildArgs,
   },
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/files.ts',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/ui/assets/__mocks__/files.ts',
     '\\.css$': 'identity-obj-proxy',
   },
   rootDir: path.join(__dirname, '../'),
