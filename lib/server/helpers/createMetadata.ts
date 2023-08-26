0405cf1e3b5ede39e755a7341f25eec156f30b31
Andrew Wei <andrew@sybl.io>
2023-08-26 07:41:12 -0700

    Remove src dependency in lib

--- lib/server/helpers/createMetadata.ts
+++ lib/server/helpers/createMetadata.ts
@@ -1,22 +1,21 @@
-import { Request } from 'express'
 import { RouteObject, matchRoutes } from 'react-router'
-import routes from '../../../src/routes.conf'
 import { I18nConfig, createGetLocalizedString, createResolveLocaleOptions, resolveLocaleFromURL } from '../../i18n'
 import { joinURL } from '../../utils'
 
 type Options = {
-  i18n: I18nConfig
   baseURL: string
+  i18n: I18nConfig
+  routes: RouteObject[]
 }
 
-export default async function createMetadata(req: Request, { baseURL, i18n }: Options) {
-  const resolveResult = resolveLocaleFromURL(req.url, createResolveLocaleOptions(i18n))
-  const matchedRouteObject = matchRoutes(routes, req.url)?.[0]?.route as RouteObject
+export default async function createMetadata(url: string, { baseURL, i18n, routes }: Options) {
+  const resolveResult = resolveLocaleFromURL(url, createResolveLocaleOptions(i18n))
+  const matchedRouteObject = matchRoutes(routes, url)?.[0]?.route as RouteObject
   const metadata = await matchedRouteObject?.metadata?.(createGetLocalizedString(resolveResult?.locale, i18n))
 
   return {
     ...metadata ?? {},
     locale: resolveResult?.locale,
-    url: joinURL(baseURL, req.url),
+    url: joinURL(baseURL, url),
   }
 }
