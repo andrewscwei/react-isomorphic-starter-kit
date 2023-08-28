c1fe5d07861f4f8d80501c575b776cdd8e04bcc9
Andrew Wei <andrew@sybl.io>
2023-08-28 14:43:12 -0700

    Add tryOrUndefinedAsync

--- lib/utils/tryOrUndefined.ts
+++ lib/utils/tryOrUndefined.ts
@@ -15,3 +15,21 @@ export function tryOrUndefined<T>(fn: () => T): T | undefined {
     return undefined
   }
 }
+
+/**
+ * Executes an asyncfunction in a try/catch block and returns the return value
+ * if no errors are thrown, `undefined` otherwise.
+ *
+ * @param fn The function to execute.
+ *
+ * @returns The return value of `fn` if no errors are thrown, `undefined`
+ *          otherwise.
+ */
+export async function tryOrUndefinedAsync<T>(fn: () => Promise<T>): Promise<T | undefined> {
+  try {
+    return await fn()
+  }
+  catch (err) {
+    return undefined
+  }
+}
