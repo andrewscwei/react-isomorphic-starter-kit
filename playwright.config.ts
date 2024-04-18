71b8969ec6c8052c1554232d68cfeb0536d87723
Andrew Wei <andrewscwei@gmail.com>
2024-04-17 20:18:15 -0700

    Build: Increase Playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? 'null' : 'html',
+  timeout: 60000,
   use: {
     baseURL: 'http://localhost:8080',
     trace: 'on-first-retry',
