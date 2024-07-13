3aaa8274269e08a15f2b4018ed56aa55fb3cf776
Andrew Wei <andrewscwei@gmail.com>
2024-07-13 11:14:40 -0700

    fix: Server entry file

--- src/main.server.tsx
+++ src/main.server.tsx
@@ -5,7 +5,6 @@
 import { generateLocalizedRoutes } from '@lib/i18n'
 import { createMetadata } from '@lib/layouts'
 import { generateRobots, generateSitemap } from '@lib/seo'
-import { type RenderFunction, type RobotsBuilder, type SitemapBuilder } from '@lib/server'
 import { renderToPipeableStream, type RenderToPipeableStreamOptions } from 'react-dom/server'
 import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router-dom/server'
 import { BASE_PATH, BASE_URL, DESCRIPTION, MASK_ICON_COLOR, PUBLIC_URL, THEME_COLOR, TITLE } from './app.conf'
@@ -16,11 +15,11 @@ import { App } from './ui/App'
 
 const localizedRoutes = generateLocalizedRoutes(routes, i18n)
 
-export const robots: RobotsBuilder = () => generateRobots(localizedRoutes, seo)
+export const robots = () => generateRobots(localizedRoutes, seo)
 
-export const sitemap: SitemapBuilder = () => generateSitemap(localizedRoutes, seo)
+export const sitemap = () => generateSitemap(localizedRoutes, seo)
 
-export const render: RenderFunction = async req => {
+export const render = async (req: Request) => {
   const handler = createStaticHandler(localizedRoutes, { basename: BASE_PATH })
   const context = await handler.query(req)
   if (context instanceof Response) throw Error('Redirect response from static handler')
