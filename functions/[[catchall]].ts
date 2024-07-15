7be61e23c11559bb61424ee41fc0f306a26467d5
Andrew Wei <andrewscwei@gmail.com>
2024-07-15 00:17:07 -0700

    feat: Consolidate handlers into one for edge

--- functions/[[catchall]].ts
+++ functions/[[catchall]].ts
@@ -3,7 +3,16 @@ import * as module from '../build/main.edge'
 import { renderRoot } from '../lib/esr/renderRoot'
 
 export const onRequest: PagesFunction = async ({ request, functionPath: path }) => {
-  const response = await renderRoot(module, template)(request, path)
-
-  return response
+  switch (path) {
+    case '/robots.txt':
+      return new Response(await module.robots(), {
+        headers: { 'content-type': 'text/plain' },
+      })
+    case '/sitemap.xml':
+      return new Response(await module.sitemap(), {
+        headers: { 'Content-Type': 'application/xml' },
+      })
+    default:
+      return renderRoot(module, template)(request, path)
+  }
 }
