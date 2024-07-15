781d0e912a286a871e3a202106c969fa3ede5078
Andrew Wei <andrewscwei@gmail.com>
2024-07-15 00:17:07 -0700

    feat: Consolidate handlers into one for edge

--- functions/robots.txt.ts
+++
@@ -1,9 +0,0 @@
-import { robots } from '../build/main.edge'
-
-export const onRequest: PagesFunction = async ({ request }) => {
-  const res = await robots()
-
-  return new Response(res, {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
--- functions/sitemap.xml.ts
+++
@@ -1,9 +0,0 @@
-import { sitemap } from '../build/main.edge'
-
-export const onRequest: PagesFunction = async ({ request }) => {
-  const res = await sitemap()
-
-  return new Response(res, {
-    headers: { 'Content-Type': 'application/xml' },
-  })
-}
