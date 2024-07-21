f487409aa9c17ab807fb8a2149d07a473e8738b6
Andrew Wei <andrewscwei@gmail.com>
2024-07-21 12:17:20 -0700

    refactor: App config

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,17 +2,11 @@
  * @file Application config.
  */
 
-/** URL of the app. */
+/** Build args */
 export const BASE_URL = import.meta.env.BASE_URL
-
-/** Base path of the router (i.e. the `basename` property). */
 export const BASE_PATH = import.meta.env.BASE_PATH
-
-/** App version. */
 export const VERSION = `v${import.meta.env.VERSION}+build.${import.meta.env.BUILD_NUMBER}`
-
-/** Default locale. */
 export const DEFAULT_LOCALE = import.meta.env.DEFAULT_LOCALE
-
-/** Default metadata. */
 export const DEFAULT_METADATA = import.meta.env.DEFAULT_METADATA
+
+/** App config */
