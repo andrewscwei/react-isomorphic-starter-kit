5629a50beaba45e19aa2640eb993061763458f54
Andrew Wei <andrewscwei@gmail.com>
2025-10-20 16:42:22 +0900

    fix: Fix favicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useFavicon } from '@lib/meta'
 import { StrictMode, type PropsWithChildren } from 'react'
-import { BASE_PATH } from './app.config.js'
 import './styles/theme.css'
 
 type Props = PropsWithChildren
 
 export function App({ children }: Readonly<Props>) {
-  useFavicon({
-    icon: {
-      darkImage: `${BASE_PATH.replace(/\/+$/, '')}/favicon-dark.svg`,
-    },
-    alternateIcon: {
-      darkImage: `${BASE_PATH.replace(/\/+$/, '')}/favicon-dark.png`,
-    },
-  })
-
   return (
     <StrictMode>
       {children}
