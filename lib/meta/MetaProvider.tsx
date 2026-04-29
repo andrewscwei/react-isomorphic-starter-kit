62c083d252eb7d9d9668ab49249d3c6718e1c3bb
Andrew Wei <andrewscwei@gmail.com>
2026-04-28 22:51:23 -0700

    refactor: Rename MetaProviderProps to Props for consistency

--- lib/meta/MetaProvider.tsx
+++ lib/meta/MetaProvider.tsx
@@ -3,7 +3,7 @@ import { type PropsWithChildren, useMemo } from 'react'
 import { MetaContext } from './MetaContext.js'
 import { type Metadata } from './types/Metadata.js'
 
-type MetaProviderProps = PropsWithChildren<{
+type Props = PropsWithChildren<{
   metadata?: Metadata
 }>
 
@@ -11,11 +11,11 @@ type MetaProviderProps = PropsWithChildren<{
  * Context provider that holds a reference to the data for meta tags during
  * server-side rendering or edge-side rendering.
  *
- * @param props See {@link MetaProviderProps}.
+ * @param props See {@link Props}.
  *
  * @returns The context provider.
  */
-export function MetaProvider({ children, metadata }: MetaProviderProps) {
+export function MetaProvider({ children, metadata }: Props) {
   const value = useMemo(() => ({ metadata }), [metadata])
 
   return (
